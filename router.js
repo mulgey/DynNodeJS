161bd03a13e9bd67828efb8a9c3ca382e2ecce99
knh369 <knh369@konyadh.gov.tr>
2018-11-02 16:08:09 +0300

    daily edu

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+var profile = require("./profile");
+
 //Handle HTTP route GET / and POST / i.e. Home
 function home (req, res) {
     //if url == "/" && GET
@@ -22,14 +24,32 @@ function home (req, res) {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
       res.write("Header\n");
-      res.write(username + "\n");
-      res.end("Footer\n");
+
       //get json from Treehouse
-        //on "end"
-          //show profile
-        //on "error"
-          //show error
-  
+      var studentProfile = new profile(username);
+      //on "end"
+      studentProfile.on("end", function(profileJSON) {
+        //show profile
+
+        //store the values we need
+        var values = {
+          avatarUrl: profileJSON.gravatar_url,
+          username: profileJSON.profile_name,
+          badges: profileJSON.badges.length,
+          javascriptPoints: profileJSON.points.JavaScript 
+        }
+
+        // simple response
+        res.write(`${values.username} has ${values.badges} badges.` + "\n");
+        res.end("Footer\n");
+      });
+
+      //on "error"
+      studentProfile.on("error", function(error){
+        //show error
+        res.write(error.message + "\n");
+        res.end("Footer\n");
+      });
     }
   }
 
